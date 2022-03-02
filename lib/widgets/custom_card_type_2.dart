0f3e4586a2ba0be6ba37c281923d0113c555774b
Ruben Pacheco <rpacheco@movisat.com>
2022-03-02 13:29:53 +0100

    añadir propiedades a la clase de card personalizada, una propiedad la ruta de la url de la imagen obligatoria o required y un valor opcional para el título de la imagen

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,11 @@ import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  const CustomCardType2({this.nameCard, required this.imageUrl, Key? key})
+      : super(key: key);
+
+  final String imageUrl;
+  final String? nameCard;
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +16,20 @@ class CustomCardType2 extends StatelessWidget {
         shadowColor: AppTheme.colorPrimary.withOpacity(0.4),
         elevation: 10,
         child: Column(children: <Widget>[
-          const FadeInImage(
-            image: NetworkImage(
-                'https://almanatura.com/wp-content/uploads/2015/03/emprendedor_ambiental-1-1200x799.jpeg?p=8725'),
-            placeholder: AssetImage('assets/loading_fl_components.gif'),
+          FadeInImage(
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('assets/loading_fl_components.gif'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: const Text('Un hermoso amanecer.'),
-          ),
+          if (nameCard != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: Text(nameCard!),
+            ),
         ]));
   }
 }
