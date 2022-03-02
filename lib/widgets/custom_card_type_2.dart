f4ef86801d7ec97504ee616b4c2b605b9a5a5022
Ruben Pacheco <rpacheco@movisat.com>
2022-03-02 12:32:20 +0100

    añadir texto a una imagen, alinear texto, añadir estilos a la card como border redondeados, sombras personalizadas

--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
@@ -6,12 +7,24 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+        shadowColor: AppTheme.colorPrimary.withOpacity(0.4),
         elevation: 10,
-        child: Column(children: const <Widget>[
-          FadeInImage(
+        child: Column(children: <Widget>[
+          const FadeInImage(
             image: NetworkImage(
                 'https://almanatura.com/wp-content/uploads/2015/03/emprendedor_ambiental-1-1200x799.jpeg?p=8725'),
             placeholder: AssetImage('assets/loading_fl_components.gif'),
+            width: double.infinity,
+            height: 230,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+            child: const Text('Un hermoso amanecer.'),
           ),
         ]));
   }
