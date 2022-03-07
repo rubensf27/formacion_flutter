14ad531c731378cedd4b11cb2d25fde5f76c39a8
Ruben Pacheco <rpacheco@movisat.com>
2022-03-07 09:25:36 +0100

    visualizar distintos check y switch widget que manejan datos booleanos para realizar ciertas acciones, ver las licencias y librerías utilizadas por la app con la clase o widget AboutListTile

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -10,38 +10,72 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Slider & Checks Screen'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 50,
-              max: 400,
-              value: _sliderValue,
-              activeColor: AppTheme.colorPrimary,
-              //divisions: 10,
-              onChanged: (value) {
-                _sliderValue = value;
-                setState(() {});
-                print(value);
-              },
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max: 400,
+            value: _sliderValue,
+            activeColor: AppTheme.colorPrimary,
+            //divisions: 10,
+            onChanged: _sliderEnabled
+                ? (value) {
+                    _sliderValue = value;
+                    setState(() {});
+                    print(value);
+                  }
+                : null,
+          ),
+          /** Checkbox(
+            value: _sliderEnabled,
+            onChanged: (value) {
+              _sliderEnabled = value ?? true;
+              setState(() {});
+            },
+          ),*/
+          /** CheckboxListTile(
+            value: _sliderEnabled,
+            activeColor: AppTheme.colorPrimary,
+            title: const Text('Habilitar Slider'),
+            onChanged: (value) => setState(() {
+              _sliderEnabled = value ?? true;
+            }),
+          ),*/
+          /**Switch(
+            value: _sliderEnabled,
+            activeColor: AppTheme.colorPrimary,
+            onChanged: (value) {
+              _sliderEnabled = value;
+              setState(() {});
+            },
+          ),*/
+          SwitchListTile.adaptive(
+            title: const Text('Habilitar Slider'),
+            activeColor: AppTheme.colorPrimary,
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+              _sliderEnabled = value;
+            }),
+          ),
+          const AboutListTile(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1jFDtkcDL1jGX0a8qb_E5Hoxt4JORH805yw&usqp=CAU'),
+                fit: BoxFit.contain,
+                width: _sliderValue,
+              ),
             ),
-            Image(
-              image: const NetworkImage(
-                  'https://png.pngtree.com/png-clipart/20220130/ourmid/pngtree-watercolor-flower-wreath-from-dried-palm-leaves-and-roses-png-image_4377326.png'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
